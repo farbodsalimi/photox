7f11d5712776b2fc1c7d32c3bc07b5757adb6b03
Farbod Salimi <4585739+farbodsalimi@users.noreply.github.com>
2020-02-17 13:43:15 -0800

    Init tests

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/google/subcommands v1.2.0
 	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
 	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.2.2
 )
---
+++ tests/src/util/directory_test.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"photox/src/util"
+)
+
+func TestMakePathByTakenDateTime(t *testing.T) {
+	dt := time.Unix(1581975738, 0)
+	val := util.MakePathByTakenDateTime("base_path", dt)
+	assert.Equal(t, val, "base_path/2020-2-17")
+}
